47fe5eb5cf61de1d652eed4972b244c6bd722de7
Andreas Skaar <andreas.skaar@live.com>
2013-06-21 15:49:43 +0200

    cf

--- Core/SyncTcpClientSession.cs
+++ Core/SyncTcpClientSession.cs
@@ -12,7 +12,7 @@ namespace SuperSocket.ClientEngine
 	public class SyncTcpClientSession : TcpClientSession
 	{
 		//OnDataReceived call this when we get data
-		private Socket _socket;
+		
 		private byte[] _buffer = new byte[1024];
 
 		public SyncTcpClientSession(EndPoint remoteEndPoint)
@@ -35,32 +35,34 @@ namespace SuperSocket.ClientEngine
 		{
 			foreach (var item in items)
 			{
-				_socket.Send(item.Array);
+				Client.Send(item.Array);
 			}
 		}
 
 		public override void Connect()
 		{
-			_socket = new Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
-			_socket.SetSocketOption(System.Net.Sockets.SocketOptionLevel.Tcp, System.Net.Sockets.SocketOptionName.NoDelay, 1);
-			_socket.Connect(RemoteEndPoint);
-			if (_socket.Connected == false)
+			Client = new Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Stream, System.Net.Sockets.ProtocolType.Tcp);
+			Client.SetSocketOption(System.Net.Sockets.SocketOptionLevel.Tcp, System.Net.Sockets.SocketOptionName.NoDelay, 1);
+			Client.Connect(RemoteEndPoint);
+			if (Client.Connected == false)
 				throw new Exception("socket timed out");
 
-			_socket.BeginReceive(_buffer, 0, _buffer.Length, System.Net.Sockets.SocketFlags.None, OnDataReceived, _buffer);
+			OnConnected();
+
+			Client.BeginReceive(_buffer, 0, _buffer.Length, System.Net.Sockets.SocketFlags.None, OnDataReceived, _buffer);
 		}
 
 		private void OnDataReceived(IAsyncResult ar)
 		{
 			try
 			{
-				var bytes = _socket.EndReceive(ar);
+				var bytes = Client.EndReceive(ar);
 
 				if (bytes > 0)
 				{
 					var data = _buffer;
 					_buffer = new byte[1024];
-					_socket.BeginReceive(_buffer, 0, _buffer.Length, System.Net.Sockets.SocketFlags.None, OnDataReceived, _buffer);
+					Client.BeginReceive(_buffer, 0, _buffer.Length, System.Net.Sockets.SocketFlags.None, OnDataReceived, _buffer);
 					OnDataReceived(data, 0, bytes);
 				}
 			}
